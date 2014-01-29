baa6d24d55843eddb4ba7ac9e22856936f08791e
Eric Mrak <mail@ericmrak.info>
2014-01-28 20:44:10 -0800

    can checkout

--- src/book.js
+++ src/book.js
@@ -1,5 +1,14 @@
 module.exports = function(title, author){
   this.title = title;
   this.author = author;
+
+  this.isAvailable = function(){
+    return this.borrower == null;
+  };
+
+  this.checkout = function(borrower) {
+    this.borrower = borrower;
+  };
+
   return this;
 };
