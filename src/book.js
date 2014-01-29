896838371294386e079e51cf97adaff78a528cd3
Eric Mrak <mail@ericmrak.info>
2014-01-28 20:50:11 -0800

    made borrwer a private variable
    Conflicts:
    	src/book.js

--- src/book.js
+++ src/book.js
@@ -1,5 +1,15 @@
 module.exports = function(title, author){
+  var borrower;
   this.title = title;
   this.author = author;
+
+  this.isAvailable = function(){
+    return borrower == null;
+  };
+
+  this.checkout = function(name) {
+    borrower = name;
+  };
+
   return this;
 };
